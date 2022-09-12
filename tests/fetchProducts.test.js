56df86814aba5733c0a4486773a4165bdc5bab49
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-09-12 16:52:22 -0300

    Testando requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,5 +4,21 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
   // implemente seus testes aqui
+  it('Verifica se fetchProducts é uma função'), () => {
+    const resultado = typeof fetchProducts;
+    expect(resultado).toBe('function');
+  }
+  it("Execute a função fetchProducts com o argumento 'computador' e teste se fetch foi chamada"), async () => {
+    await fetchProducts('computador')
+    expect(fetch).toBeCalled();
+  }
+  it("Execute a função fetchProducts com o argumento 'computador' é uma estrutura de dados igual ao objeto computadorSearch"), async () => {
+    const resultado = await fetchProducts('computador')
+    expect(resultado).toMatchObject(computadorSearch);
+  }
+  it("Se ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: 'You must provide an url'"), async () => {
+    const resultado = await fetchProducts();
+    expect(resultado).toThrow(new Error ('You must provide an url'));
+  }
   fail('Teste vazio');
 });
