7645f8510b7ebdb506c30766e081c31e57f4f6b5
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2022-09-14 16:13:31 -0300

    Concluído requisito 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -1,9 +1,16 @@
 const localStorageSimulator = require('../mocks/localStorageSimulator');
 const getSavedCartItems = require('../helpers/getSavedCartItems');
+// const { expect } = require('chai');
 
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  // fail('Teste vazio');
+  test(('se, ao executar getSavedCartItems, o método localStorage.getItem é chamado'), async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalled();
+  })
+  test(('se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro'), async () => {
+    await getSavedCartItems();
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  })
 });
